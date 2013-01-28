63049cb817922ed130dfe7bfcd28091a5fa0ed80
dicampus <dicampus@dicampus.dicampus>
2013-01-28 10:08:58 +0100

    Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ajax::Application.config.secret_token = '8c05f0c284d7d78eab67bec5288da9e81abc9ffbb056820855fb257027a092bb377f7fc9e110a943c087ade474732f9ee9440891c56eb7de79ed41f4bcc1316c'
